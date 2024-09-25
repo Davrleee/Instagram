7c20bebf3f505615e4d10f8ddbe6ccf9db9c5f24
Davrleee <andreasz.david.leginusz@gmail.com>
2024-09-25 14:05:16 +0200

    .env updated

--- src/app/o-mne/page.tsx
+++ src/app/o-mne/page.tsx
@@ -5,7 +5,7 @@
 import Typography from "@mui/material/Typography"
 import Container from "@mui/material/Container"
 
-export default function AboutMe() {
+export default function About() {
   
     return(
         <Container>
